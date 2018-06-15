7ab77d83ee78249774969f4c38df05d041711192
aas86 <aas86@mail.ru>
2018-06-15 10:25:26 +0700

    Задача Квадратное уравнение 1. Коэффициенты могут быть вещественными; 2. При повторном нажатии на кнопку старый текст стирается; 3. Если ввели что-то некорректное, выводится сообщение об ошибке; 4. Рассмотрен случай, когда a =0; 5. Добавил normilize.css; 6. Всегда выводится дискриминант.

--- QuadraticEquation/src/quadraticEquation.js
+++ QuadraticEquation/src/quadraticEquation.js
@@ -1,45 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var button = document.getElementById("button");
-    button.addEventListener("click", getData);
+        var button = document.getElementById("button");
+        button.addEventListener("click", calculate);
 
-    function getData() {
-        var a = document.getElementById("a").value;
-        var b = document.getElementById("b").value;
-        var c = document.getElementById("c").value;
-        a = parseInt(a, 10);
-        if (isNaN(a)) {
-            a = 0;
+        function calculate() {
+            var a = document.getElementById("a").value;
+            var b = document.getElementById("b").value;
+            var c = document.getElementById("c").value;
+            a = parseFloat(a);
+            b = parseFloat(b);
+            c = parseFloat(c);
+            if (isNaN(a) || isNaN(b) || isNaN(c)) {
+                alert("Ввели фигню");
+            } else {
+                var x1;
+                var x2;
+                var result1 = document.getElementById("result1");
+                var result2 = document.getElementById("result2");
+                var D = document.getElementById("d");
+                if (a === 0) {
+                    x1 = -c / b;
+                    result1.innerHTML = x1;
+                    result2.innerHTML = "";
+                } else {
+                    var d = b * b - 4 * a * c;
+                    if (d < 0) {
+                        D.innerHTML = "D = " + d;
+                        result1.innerHTML = "Нет корней";
+                        result2.innerHTML = "";
+                    } else if (d === 0) {
+                        x1 = -b / (2 * a);
+                        D.innerHTML = "D = " + d;
+                        result1.innerHTML = "x1 = " + x1;
+                        result2.innerHTML = "";
+                    } else {
+                        x1 = (-b + Math.sqrt(d)) / (2 * a);
+                        x2 = (-b - Math.sqrt(d)) / (2 * a);
+                        D.innerHTML = "D = " + d;
+                        result1.innerHTML = "x1 = " + x1;
+                        result2.innerHTML = "x2 = " + x2;
+                    }
+                }
+            }
         }
-        b = parseInt(b, 10);
-        if (isNaN(b)) {
-            b = 0;
-        }
-        c = parseInt(c, 10);
-        if (isNaN(c)) {
-            c = 0;
-        }
-        var d = b * b - 4 * a * c;
-        var x1;
-        var x2;
-        var result1;
-        var result2;
-        if (d < 0) {
-            x1 = document.createElement("p");
-            x1.innerHTML = "Корней нет т.к. D = " + d;
-            document.body.appendChild(x1);
-        } else if (d === 0) {
-            x1 = -b / (2 * a);
-            result1 = document.createElement("p");
-            result1.innerHTML = "D =" + d + "  " + "x1 = " + x1;
-        } else {
-            x1 = (-b + Math.sqrt(d)) / (2 * a);
-            x2 = (-b - Math.sqrt(d)) / (2 * a);
-            result1 = document.createElement("p");
-            result1.innerHTML = "x1 = " + x1;
-            result2 = document.createElement("p");
-            result2.innerHTML = "x2 = " + x2;
-        }
-        document.body.appendChild(result1);
-        document.body.appendChild(result2);
     }
-});
+);
