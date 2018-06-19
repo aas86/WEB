d3523939840dcdf8ee9b8ff4875448df43ae927c
aas86 <aas86@mail.ru>
2018-06-19 13:22:12 +0700

    TODOList 1. Добавил пустые строки; 2. Убрал лишний код; 3. Если ничего не ввели, то ничего не добавляется.

--- TODOList/src/ToDo.js
+++ TODOList/src/ToDo.js
@@ -5,19 +5,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     button.addEventListener("click", function () {
         var text = input.value;
-        var li = document.createElement("li");
-        li.innerHTML = text;
-        var deleteButton = document.createElement("button");
-        deleteButton.innerHTML = "delete";
-        deleteButton.setAttribute("style", "background-color : green");
-        list.appendChild(li);
-        li.appendChild(deleteButton);
-        input.value = "";
+
+        if (text !== ""){
+            var li = document.createElement("li");
+            li.innerHTML = text;
+
+            var deleteButton = document.createElement("button");
+            deleteButton.innerHTML = "delete";
+            deleteButton.style.color = "red";
+            deleteButton.style.backgroundColor = "black";
+
+
+            list.appendChild(li);
+            li.appendChild(deleteButton);
+        } else{
+            return;
+        }
+
         deleteButton.addEventListener('click', function () {
-            var listItem = document.getElementsByTagName("li");
             li.remove();
-            deleteButton.remove();
-        })
+        });
+        input.value = "";
     });
 
 });
