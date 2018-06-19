003fe20358c9317e18eb2a60e321b15a587588d8
aas86 <aas86@mail.ru>
2018-06-19 13:54:04 +0700

    TODOList Переделал условие.

--- TODOList/src/ToDo.js
+++ TODOList/src/ToDo.js
@@ -6,21 +6,19 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function () {
         var text = input.value;
 
-        if (text === ""){
+        if (text === "") {
             return;
         }
-            var li = document.createElement("li");
-            li.innerHTML = text;
+        var li = document.createElement("li");
+        li.innerHTML = text;
 
-            var deleteButton = document.createElement("button");
-            deleteButton.innerHTML = "delete";
-            deleteButton.style.color = "red";
-            deleteButton.style.backgroundColor = "black";
+        var deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "delete";
+        deleteButton.style.color = "red";
+        deleteButton.style.backgroundColor = "black";
 
-
-            list.appendChild(li);
-            li.appendChild(deleteButton);
-
+        list.appendChild(li);
+        li.appendChild(deleteButton);
 
         deleteButton.addEventListener('click', function () {
             li.remove();
