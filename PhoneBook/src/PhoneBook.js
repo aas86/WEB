7c82ef54e62d037dbd4d0c51fb22f036eb92c607
aas86 <duxa19861@gmail.com>
2018-06-22 18:25:57 +0700

    Phone book Сделал подсветку красным незаполненых полей

--- PhoneBook/src/PhoneBook.js
+++ PhoneBook/src/PhoneBook.js
@@ -6,35 +6,32 @@ $(document).ready(function () {
         var surname = $("#surname").val();     // Получил значение поля ФАМИЛИЯ
         var phone = $("#tel-number").val();    // Получил значение поля №телефона
 
-        var form = $(".contact-form");
-        if (name === "") {
-            form.find("#name").style();
-            return;
-        } else if (surname === "") {
-            form.find("#surname").style.borderColor = "red";
-            return;
-        } else if (phone === "") {
-            form.find("#phone").style.borderColor = "red";
-            return;
+        if(name !== "" && surname !== "" && phone !== ""){
+            var tr = $("<tr><th></th><th></th><th></th><th></th>" +
+                    "<th><button id='delete-button' type='button'> Удалить</button></th></tr>"); // Создаём строку
+                setInput();
+                tr.find("th:eq(1)").text(name);
+                tr.find("th:eq(2)").text(surname);
+                tr.find("th:eq(3)").text(phone);
+                /*tr.find("th:eq(0)").text(id);
+                id++;*/
+
+                $(".table").find("tbody").append(tr);
+                setRowsNumber();
+        } else{
+                var form = $(".contact-form input");
+                form.each(function(){
+                    var temp = $(this).val();
+                    if(temp === ""){
+                    $(this).css({"background-color": "#FFDAB9"});
+                    }
+                });
         }
-
-
-        var tr = $("<tr><th></th><th></th><th></th><th></th>" +
-            "<th><button id='delete-button' type='button'> Удалить</button></th></tr>");
-        tr.find("th:eq(1)").text(name);
-        tr.find("th:eq(2)").text(surname);
-        tr.find("th:eq(3)").text(phone);
-        /*tr.find("th:eq(0)").text(id);
-        id++;*/
-
-        $(".table").find("tbody").append(tr);
-        setRowsNumber();
-
-        $(".contact-form").find("input").val("");
 
         tr.find("#delete-button").on("click", function () {
             $(this).closest("tr").remove();
             setRowsNumber();
+            setInput();
         });
     });
 
@@ -42,7 +39,13 @@ $(document).ready(function () {
         $(".table").find("tbody").find("tr").each(function (i, tr) {
             $(tr).find("th:eq(0)").text(i + 1);
         });
+    }
 
+    function setInput(){
+            $(".contact-form").find("input").val(""); // сброс полей ввода
+            $(".contact-form input").each(function(){
+                $(this).css({"background-color" : "#FFFfFf"});
+            });
     }
 
 });
